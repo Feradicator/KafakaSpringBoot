6ae391fa3d025263c52e15e6db4357d9a852fe1b
Feradicator <yadavshivam042001@gmail.com>
2024-06-03 02:04:12 +0530

    added json producers and consumers

---
+++ SpringBootKafka/src/main/java/com/app/controller/JsonMessageController.java
@@ -0,0 +1,28 @@
+package com.app.controller;
+
+import com.app.kafka.JsonKafkaProducer;
+import com.app.pojo.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+    @Autowired
+    private JsonKafkaProducer kafkaProducer;
+
+   
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user) {
+        kafkaProducer.sendMessage(user);
+
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+
+}
